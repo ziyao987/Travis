05a25434738198662bcd0783d8f8d0fedefe5202
ziyao987 <534331490@qq.com>
2021-05-08 13:59:21 +0200

    key

--- run_calabash.sh
+++ run_calabash.sh
@@ -1,3 +1,3 @@
-cp ./app/build/outputs/apk/debug/app-debug.apk ./calabah-test-android/app.apk
+cp ./app/build/outputs/apk/release/app-release.apk ./calabah-test-android/app.apk
 cd ./calabah-test-android/
 calabash-android run app.apk
