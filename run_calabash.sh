704ca6ca74da5e25586dd5b39c6395290bc620d2
ziyao <79931561+ziyao987@users.noreply.github.com>
2021-05-08 17:13:50 +0200

    Update run_calabash.sh

--- run_calabash.sh
+++ run_calabash.sh
@@ -1,3 +1,3 @@
 cp ./app/build/outputs/apk/release/app-release.apk ./calabash-test-android/app.apk
 cd ./calabash-test-android/
-bundle exec calabash-android run app.apk
+calabash-android run app.apk
